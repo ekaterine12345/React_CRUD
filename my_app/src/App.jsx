a332ad7460971b6c45e0d3afb7ff3b4129950c43
ekaterine gurgenidze <ekateinegurgenidze@gmail.com>
2024-06-15 22:47:57 +0400

    my CRUD

---
+++ my_app/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './Home'
+import Create from './Create'
+import Update from './Update'
+import Read from './Read'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  
+
+  return (
+
+
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home/>}> </Route>
+            <Route path="/create" element={<Create/>}> </Route>
+            <Route path="/update/:id" element={<Update/>}> </Route>
+            <Route path="/read/:id" element={<Read/>}> </Route>
+        </Routes>
+    </BrowserRouter>
+
+      
+  )
+}
+
+export default App
